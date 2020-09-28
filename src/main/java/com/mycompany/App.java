8e1b0f676ab051157d58b64c8237190d997e23f4
dimmxx <dimmxx@gmail.com>
2020-09-28 22:07:51 +0300

    time

--- src/main/java/com/mycompany/App.java
+++ src/main/java/com/mycompany/App.java
@@ -7,9 +7,8 @@ import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class App {
 
@@ -30,12 +29,22 @@ public class App {
             String month = date.getMonth().getValue() < 10?"0" + date.getMonth().getValue():"" + date.getMonth().getValue();
             String day = (i + 1) < 10?"0" + (i + 1):"" + (i + 1);
 
-
-
-
-
             map.put(date.getYear() + "-" + month + "-" + day, null);
         }
+
+        List<String> ll = map.keySet().stream()
+                .filter(k -> LocalDate.parse(k).getDayOfWeek().getValue() == 6)
+                .collect(Collectors.toList());
+
+        map.forEach((k, v) -> System.out.println(k + ": " + v));
+
+        System.out.println(ll);
+
+
+        map.entrySet().stream()
+                .filter(e -> LocalDate.parse(e.getKey()).getDayOfWeek().getValue() == 6)
+                .forEach(e -> e.setValue(LocalTime.now()));
+
 
         map.forEach((k, v) -> System.out.println(k + ": " + v));
 
