2d2a2c1e9a04e3b0f85d62201733f73418307e61
mint <mint@mac.local>
2020-12-26 23:04:20 +0200

    mac

--- src/main/java/com/mycompany/App.java
+++ src/main/java/com/mycompany/App.java
@@ -24,7 +24,7 @@ public class App {
 //        }
 //        System.out.println(list);
 
-        List<LocalDateTime> result = new ArrayList<>();
+        List<Pair<LocalDateTime, LocalDateTime>> result = new ArrayList<>();
 
         List<String> stringList = List.of(
 
@@ -38,20 +38,35 @@ public class App {
                 "2020-12-25T09:10"
                 );
         List<LocalDateTime> list = stringList.stream().map(LocalDateTime::parse).collect(Collectors.toList());
-        result.add(list.get(0));
-        result.add(list.get(list.size() - 1));
-        for (int i = 0; i < list.size() - 1; i++){
-            if(!Objects.equals(list.get(i).plus(1, ChronoUnit.MINUTES), list.get(i + 1))){
-                result.add(list.get(i));
-                result.add(list.get(i + 1));
+
+
+
+        String s = "916 917 918 919 931 949 1011 1012 1013 1014 1015 1016 1017 1018 1019 1020";
+
+
+        int b = 0;
+        int loop = 0;
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (!Objects.equals(list.get(i).plus(1, ChronoUnit.MINUTES), list.get(i + 1))) {
+                if(loop > 1){
+                    Pair<LocalDateTime, LocalDateTime> pair = new Pair<>(list.get(b), list.get(i));
+                    result.add(pair);
+                    loop = 0;
+                    b = i + 1;
+
+                }
+
+            }else{
+                if(i == list.size() - 1){
+                    Pair<LocalDateTime, LocalDateTime> pair = new Pair<>(list.get(b), list.get(i + 1));
+                    result.add(pair);
+                    break;
+                }
+                loop++;
+
             }
         }
-
-        result = result.stream().distinct().sorted().collect(Collectors.toList());
-        System.out.println(result);
-
-
-
 
 
 
