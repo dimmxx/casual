30b6ee2e32299c5a3dc4f5fe47e940469dfa0f02
dimmxx <dimmxx@gmail.com>
2021-02-19 13:47:28 +0200

    async

--- src/main/java/com/mycompany/App.java
+++ src/main/java/com/mycompany/App.java
@@ -1,95 +1,51 @@
 package com.mycompany;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 public class App {
 
     public static void main(String[] args) {
 
-        String str = "2020-12-25 09:00";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        LocalDateTime localDateTimeS = LocalDateTime.parse(str, formatter);
 
-//        List<LocalDateTime> list = new ArrayList<>();
-//        list.add(localDateTimeS);
-//        for(int i = 0; i < 10; i++){
-//            localDateTimeS = localDateTimeS.plus(30, ChronoUnit.MINUTES);
-//            list.add(localDateTimeS);
-//        }
-//        System.out.println(list);
-
-        List<Pair<LocalDateTime, LocalDateTime>> result = new ArrayList<>();
-
-        List<String> stringList = List.of(
-
-                "2020-12-25T09:01",
-
-                "2020-12-25T09:04",
-                "2020-12-25T09:05",
-                "2020-12-25T09:06",
+        String req = "https://fttx-client-dev.impltech.com/ogc/1?ACCEPTVERSIONS=2.0.0,1.1.0,1.0.0&SERVICE=WFS&REQUEST=GetFeature&VERSION=1.1.0&TYPENAME=cable&outputFormat=application/json&SRSNAME=EPSG:4326";
 
 
-                "2020-12-25T09:10"
-                );
-        List<LocalDateTime> list = stringList.stream().map(LocalDateTime::parse).collect(Collectors.toList());
+        //HttpClient client = HttpClient.newHttpClient();
+
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(req))
+                .build();
+
+//        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                .thenApply(HttpResponse::body)
+//                .thenAccept(System.out::println)
+//                .join();
+
+        CompletableFuture<String> completableFuture = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body);
 
 
-
-        String s = "916 917 918 919 931 949 1011 1012 1013 1014 1015 1016 1017 1018 1019 1020";
-
-
-        int b = 0;
-        int loop = 0;
-
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (!Objects.equals(list.get(i).plus(1, ChronoUnit.MINUTES), list.get(i + 1))) {
-                if(loop > 1){
-                    Pair<LocalDateTime, LocalDateTime> pair = new Pair<>(list.get(b), list.get(i));
-                    result.add(pair);
-                    loop = 0;
-                    b = i + 1;
-
-                }
-
-            }else{
-                if(i == list.size() - 1){
-                    Pair<LocalDateTime, LocalDateTime> pair = new Pair<>(list.get(b), list.get(i + 1));
-                    result.add(pair);
-                    break;
-                }
-                loop++;
-
-            }
+        if(completableFuture.isDone()){
+            System.out.println(completableFuture);
         }
 
-
-
-
-
-
-//        List<Pair<LocalDateTime, LocalDateTime>> result = new ArrayList<>();
-//        LocalDateTime key = list.get(0);
-//        LocalDateTime value = list.get(list.size() - 1);
-//
-//        for(int i = 0; i < list.size() - 1; i++){
-//            if(!list.get(i).equals(list.get(i + 1).plus(30, ChronoUnit.MINUTES))){
-//                key = list.get(i);
-//                value = list.get(i + 1);
-//                result.add(new Pair<>(key, value));
-//                continue;
-//            }
-//
-//            value = list.get(i);
-//            result.add(new Pair<>(key, value));
-//            key = list.get(i + 1);
-//        }
-
-
     }
+
+
+
 }
---
+++ src/main/java/com/mycompany/TestBoolean.java
@@ -0,0 +1,18 @@
+package com.mycompany;
+
+public class TestBoolean {
+
+    private Boolean accepted;
+
+
+    public TestBoolean() {
+    }
+
+    public Boolean getAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(Boolean accepted) {
+        this.accepted = accepted;
+    }
+}
